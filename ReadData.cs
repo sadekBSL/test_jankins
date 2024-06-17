1ed65b75e081831acd032b4b06c54e92a36c7ee5
boussoul sadek <boussoul.sadek@gmail.com>
2024-06-17 12:20:58 +0200

    first commit

---
+++ ReadData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TestESG;
+
+namespace TestESG
+{
+    public class ReadData
+    {
+        public string file_name;
+        public string jasonstr;
+        public Client client;
+
+        public Client ReadDataFromJson()
+        {
+            file_name = "DATA.json";
+            using (StreamReader r = new StreamReader(file_name))
+            {
+                jasonstr = r.ReadToEnd();
+                client = JsonConvert.DeserializeObject<Client>(jasonstr);
+            }
+            return client;
+        }
+
+
+    }
+}
+
